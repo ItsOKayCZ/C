0ba47dfa82f92dcd7f23fa507807dd7b1d55c3cb
ItsOKayCZ <oldrich.krupauer@gmail.com>
2020-05-10 09:37:26 +0200

    Changing web

--- 2020/Web server/index.js
+++ 2020/Web server/index.js
@@ -1,1 +1,5 @@
-alert("Javascript is running. YAY");
+setTimeout(function(){
+    var el = document.createElement('p');
+    el.innerHTML = 'This DOM was created by javascript';
+    document.getElementsByTagName('body')[0].appendChild(el);
+}, 500);
